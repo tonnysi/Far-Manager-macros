079ad4357f239d59612e1b5ba1e6621ddc4030ca
Anton <progress-live@yandex.ua>
2018-12-16 22:43:00 +0300

    Added readme and gif files.

--- QuickPath.lua
+++ QuickPath.lua
@@ -4,12 +4,12 @@ Macro {
   flags="";
   action=function()
     Keys"CtrlY" -- очистить командную строку
-    local windows_clip=mf.trim(mf.clip(0)) -- скопировать буфер windows и убрать пробельные символы
-    local f_p_exist=mf.fexist(windows_clip) -- существует ли файл или папка
-    exst_in_path=mf.fsplit(windows_clip, 0x00000008) -- имеется ли в пути файл с расширением
+    local windows_clip = mf.trim(mf.clip(0)) -- скопировать буфер windows и убрать пробельные символы
+    local f_p_exist = mf.fexist(windows_clip) -- существует ли файл или папка
+    exst_in_path = mf.fsplit(windows_clip, 0x00000008) -- имеется ли в пути файл с расширением
 
     function is_path_contain_extension()
-        if exst_in_path=="" then
+        if exst_in_path == "" then
           mf.print("goto:"..windows_clip.."\\") -- доб. \ чтобы войти в папку
         else
           mf.print("goto:"..windows_clip) -- иначе к файлу
@@ -17,16 +17,16 @@ Macro {
     end;
 
     function is_system_variable()
-        local first_path_symbol=mf.substr(windows_clip, 0, 1) -- путь нач. с символа %, это систем. перемен.
-        if first_path_symbol=="%" then
-          local first_index=mf.index(windows_clip, "%"); -- систем. перемен. содержат 2 % в пути
-          del_first_symbol=mf.replace(windows_clip, "%", "", 1) -- удал. 1 вхож.
-          local second_index=mf.rindex(del_first_symbol, "%") -- наход. 2 вхож.
+        local first_path_symbol = mf.substr(windows_clip, 0, 1) -- путь нач. с символа %, это систем. перемен.
+        if first_path_symbol == "%" then
+          local first_index = mf.index(windows_clip, "%"); -- систем. перемен. содержат 2 % в пути
+          del_first_symbol = mf.replace(windows_clip, "%", "", 1) -- удал. 1 вхож.
+          local second_index = mf.rindex(del_first_symbol, "%") -- наход. 2 вхож.
           if first_index ~= -1 and second_index ~= -1 then -- если сущ. 1 и 2 вхож.
             is_path_contain_extension()
             Keys"Enter"
           end
-        elseif first_path_symbol~="%" then
+        elseif first_path_symbol ~= "%" then
           broken_path()
         else
           mf.msgbox('Unknown error with path', windows_clip)
@@ -34,16 +34,16 @@ Macro {
     end;
 
     function broken_path()
-      local notExist=mf.msgbox("Path not exist:", "\n"..windows_clip.."\nYes - close\nNo - edit path in cmd", 0x00040001)
-      if notExist==2 then -- 2 номер кнопки связан с Нет
+      local notExist = mf.msgbox("Path not exist:", "\n"..windows_clip.."\nYes - close\nNo - edit path in cmd", 0x00040001)
+      if notExist == 2 then -- 2 номер кнопки связан с Нет
         is_path_contain_extension()
       end
     end;
 
-    if f_p_exist==true then
+    if f_p_exist == true then
       is_path_contain_extension()
       Keys"Enter"
-    elseif f_p_exist==false then
+    elseif f_p_exist == false then
       is_system_variable()
     end
 
---
+++ README.md
@@ -0,0 +1,52 @@
+# Quick path Far manager macro
+
+## Description
+
+1. **Quick path** macro allows you quick navigate to path in Far Manager using windows clipboard. It is useful then you working with different programs like text editor, ide, windows explorer etc and you needed quick to open folder path in Far Manager.
+
+## Installation
+
+ 1. Far Manager (of course)
+ 2. FarCmds (standard plugin by default)
+ 3. QuickPath.lua from repo need to copy into
+> C:\Users\\{username}\AppData\Roaming\Far Manager\Profile\Macros\scripts\
+
+do not forget to put your {username}.
+
+If you use portable path may be
+
+> Far Manager\Profile\Macros\scripts\
+
+##   Usage
+#### By default hot key is ***Alt+F***. Every time you need to copy path into Windows clipboard.
+
+<br>
+
+> *Windows explorer path.*
+
+<br>
+<img src="img\quick_path_1.gif" alt="Logo Title Text 1" width="750"/>
+<br>
+
+> *Absolute path from text editor. The cursor manager can becomes on file.*
+
+<br>
+<img src="img\quick_path_2.gif" alt="Logo Title Text 2" width="750"/>
+<br>
+
+> *Windows system path variables is working.*
+
+<br>
+<img src="img\quick_path_3.gif" alt="Logo Title Text 3" width="750"/>
+<br>
+
+> *Also macro can edit the absolute path in command line if path contains mistake, but with system variables it can't.*
+
+<br>
+<img src="img\quick_path_4.gif" alt="Logo Title Text 4" width="750"/>
+<br>
+
+---
+## License
+
+[MIT license](https://opensource.org/licenses/mit-license.php "MIT license")
